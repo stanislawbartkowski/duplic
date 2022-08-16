5af0c7f43d58c86699e70d85dd4df4ce4cfd4ac9
Your Name <you@example.com>
2022-08-16 14:33:16 +0200

    ftp_password

--- dupl.sh
+++ dupl.sh
@@ -38,14 +38,15 @@ rundupl() {
     local -r OUTLOG=$3
     local -r ISSUDO=$4
     FULL="--full-if-older-than $FULLC"
-    export PASSPHRASE=$PASSPHRASE 
+    export PASSPHRASE=$PASSPHRASE
+    export FTP_PASSWORD=secret
     SUDO=
     if [ -n "$ISSUDO" ]; then
         SUDO="sudo --preserve-env=PASSPHRASE"
     fi
     log "dupl $DUHOME $DEST"
     log "$SUDO $DUPLICITY -v 5  $DUHOME $DINCLUDE  --exclude '**' $FULL --allow-source-mismatch $DEST"
-    $SUDO $DUPLICITY -v 5  $DUHOME $DINCLUDE  --exclude '**' $FULL --allow-source-mismatch "$DEST" >>$OUTLOG
+    $SUDO $DUPLICITY -v 9  $DUHOME $DINCLUDE  --exclude '**' $FULL --allow-source-mismatch "$DEST" >>$OUTLOG
     local -r RES=$?
     [ $RES -ne 0 ] && logfail "Non zero exit code $RES"
     [ $RES -eq 0 ] && log "Success"
@@ -55,7 +56,8 @@ removeoldbackup(){
     local -r DEST=$1
     local -r OUT=$2
     local -r ISSUDO=$r3
-    export PASSPHRASE=$PASSPHRASE 
+    export PASSPHRASE=$PASSPHRASE
+    export FTP_PASSWORD=secret
     if [ -n "$ISSUDO" ]; then
         SUDO="sudo --preserve-env=PASSPHRASE"
     fi
@@ -82,7 +84,7 @@ dupluser() {
     required_listofvars PASSPHRASE DINCLUDE
     local -r dest="${!what}/dupl.$user"
     local -r out=$LOGDIR/$user-$what.out
-    rundupl $home $dest $out
+    rundupl $home $dest $out 
     removeoldbackup $dest $out
 }
 
